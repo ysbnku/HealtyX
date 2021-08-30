50b2446ded4c4b3c82359b0f56363dce233c5891
Yavuz Selim Bitmez <ybitmez@nettsi.com>
2021-08-30 04:34:20 +0300

    Category request added in the apiservice

--- HealtyX/Service/APIService.swift
+++ HealtyX/Service/APIService.swift
@@ -15,16 +15,32 @@ enum APIError:String,Error {
 
 protocol APIServiceProtocol {
     func fetchHealtyFoods(complete: @escaping (_ success:Bool,_ Healty:[Healty],_ error:APIError?) ->() )
+    func fetchHealtyCategories(complete: @escaping (_ success:Bool,_ Healty:[Category],_ error:APIError?) ->() )
+
 }
 class APIService:APIServiceProtocol {
     func fetchHealtyFoods(complete: @escaping (Bool, [Healty], APIError?) -> ()) {
         let path = Bundle.main.path(forResource: "healty", ofType: "json")
         guard let filePath = path,let jsonData = try? Data(contentsOf: URL(fileURLWithPath: filePath))  else { return }
-                
+
         do
         {
             let healtyfoods = try JSONDecoder().decode([Healty].self, from: jsonData)
             complete(true,healtyfoods,nil)
+        }
+        catch let error{
+            print("Json Parse Error : \(error)")
+        }
+    }
+    
+    func fetchHealtyCategories(complete: @escaping (Bool, [Category], APIError?) -> ()) {
+        let path = Bundle.main.path(forResource: "category", ofType: "json")
+        guard let filePath = path,let jsonData = try? Data(contentsOf: URL(fileURLWithPath: filePath))  else { return}
+                
+        do
+        {
+            let categories = try JSONDecoder().decode([Category].self, from: jsonData)
+            complete(true,categories,nil)
         }
         catch let error{
             print("Json Parse Error : \(error)")
