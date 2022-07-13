3dab48cc80b6954be1a7f35f8670ff1fd410d4af
Yavuz Selim Bitmez <>
2022-07-14 02:22:14 +0300

    Refactor xcode .

---
+++ HealtyX/Extensions/UIColor+.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+.swift
+//  HealtyX
+//
+//  Created by Yavuz Selim Bitmez  on 2.07.2022.
+//
+
+import Foundation
+import UIKit
+ 
+extension UIColor {
+    class func getCustomGreenColor() -> UIColor{
+          return UIColor(red:0.38, green:0.84 ,blue:0.62 , alpha:1.00)
+      }
+}
